9e071e4f8cf9112fd137cdb867e4ba04b0983783
jhean anderson valdiviezo purizaca <jvaldiviezo@cajapiura.pe>
2024-03-07 10:36:04 -0500

    first commit

---
+++ src/main/java/com/example/chatbotapiprueba/ChatBotApiPruebaApplication.java
@@ -0,0 +1,22 @@
+package com.example.chatbotapiprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ChatBotApiPruebaApplication {
+
+
+	@GetMapping("/prueba")
+	public String mensaje(){
+		return "hola te saludo desde azure";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotApiPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatbotapiprueba/ChatBotApiPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatbotapiprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApiPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
